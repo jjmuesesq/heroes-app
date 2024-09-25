0b55b3e0a41c83d280bfa0fb9344bb3268b5e2a5
jjmuesesq <jjmuesesq@unal.edu.co>
2024-09-25 14:45:50 -0500

    :sparkles: HeroesService - Traer informacion de los heroes

--- src/app/heroes/pages/list-page/list-page.component.ts
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-list-page',
   templateUrl: './list-page.component.html',
   styles: ``
 })
-export class ListPageComponent {
+export class ListPageComponent implements OnInit {
+
+  public heroes: Hero[] = [];
+
+  constructor( private heroesService: HeroesService) {} //inyeccion del servicio
+
+  ngOnInit(): void {
+    this.heroesService.getHeroes()
+      .subscribe( heroes => this.heroes = heroes); //la propiedad de mi clase es igual a los heroes que regresa el servicio
+  }
 
 }
