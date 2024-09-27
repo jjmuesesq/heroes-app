a4b5401fd877f47ad975d8fecc2bc4d5d3285f09
jjmuesesq <jjmuesesq@unal.edu.co>
2024-09-27 18:03:47 -0500

    :sparkles: Obtener Heroe por Url

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {catchError, Observable, of} from 'rxjs';
 import { Hero } from '../interfaces/heroes.interface';
 import { environments } from '../../../environments/environments';
 
@@ -14,5 +14,14 @@ export class HeroesService {
     getHeroes(): Observable<Hero[]> {
         return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
     }
-    
+
+    getHeroById( id: string ): Observable<Hero | undefined > {
+      return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+        //capturar el error
+        .pipe(
+          //si hay error regreso un observable que retorna undefined
+          catchError( error => of(undefined) ) //of es una forma de crear Observable basado en el valor que voy a especificar
+        );
+    }
+
 }
