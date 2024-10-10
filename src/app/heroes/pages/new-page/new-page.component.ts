ca5ac4daafa7c8ac938ae5544c1a969dd533d364
jjmuesesq <jjmuesesq@unal.edu.co>
2024-10-09 19:09:19 -0500

    :sparkles: Getters dentro del componente

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
-import {Publisher} from "../../interfaces/heroes.interface";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from "@angular/forms";
+import { Hero, Publisher } from "../../interfaces/heroes.interface";
+import { HeroesService } from "../../services/heroes.service";
 
 @Component({
   selector: 'app-new-page',
@@ -24,11 +25,23 @@ export class NewPageComponent {
     { id: 'Marvel Comics', desc: 'Marvel - Comics' },
   ];
 
+  constructor( private heroesService: HeroesService){}
+
+  get currentHero(): Hero {
+    const hero = this.heroForm.value as Hero;
+    return hero;
+
+  }
+
   onSubmit(): void {
-    console.log({
+
+    /*console.log({
       forIsValid: this.heroForm.valid,
       value: this.heroForm.value,
-    });
+    });*/
+
+    if( this.heroForm.invalid ) return;
+
   }
 
 }
